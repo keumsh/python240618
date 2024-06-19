db469030463c5dbd7d27b7a351efcd590a27b503
keumsh <keum1002@gmail.com>
2024-06-19 14:13:37 +0900

    클리앙 중고장터 크롤릴

--- web2.py
+++ web2.py
@@ -9,6 +9,9 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
+#파일로 저장 (append, read, write)
+f = open("daangn.txt","a+", encoding="utf-8")
+
 posts = soup.find_all("div", attrs={"class":"card-desc"})
 for post in posts:
     titleElem = post.find("h2", attrs={"class":"card-title"})
@@ -19,6 +22,10 @@ for post in posts:
     addr = addrElem.text.strip()
     #파이썬 3/6 f-string 문법 , 변수형을 바로 넘길수 있다.
     print(f"{title},{price},{addr}")
+    f.write(f"{title},{price},{addr}\n")
+
+f.close()
+
 
 #  <div class="card-desc">
 #       <h2 class="card-title">아이폰 14 프로</h2>
